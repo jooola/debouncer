0c150fc9998f6ea15214f05d86a8a3eab39ce1e6
jo <ljonas@riseup.net>
2021-12-31 14:38:09 +0100

    fix: get static files relative to python files

--- debouncer/app.py
+++ debouncer/app.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
@@ -5,15 +7,17 @@ from starlette.responses import FileResponse
 from .router import router
 from .store import store
 
+static_path = Path(__file__).parent.parent / "web/dist"
+
 
 def create_app():
     app = FastAPI()
     app.include_router(router)
-    app.mount("/assets", StaticFiles(directory="web/dist/assets"), name="assets")
+    app.mount("/assets", StaticFiles(directory=static_path / "assets"), name="assets")
 
     @app.get("/")
     async def index():
-        return FileResponse("web/dist/index.html")
+        return FileResponse(static_path / "index.html")
 
     @app.on_event("shutdown")
     async def shutdown():
